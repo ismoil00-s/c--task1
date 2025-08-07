ac56596826bbddd946a6ee5d548f5668afdffe2a
ismoil00-s <d.sievush@gmail.com>
2025-08-07 13:37:39 +0500

    task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int max = int.MinValue;
+int n;
+while (true)
+{
+    n = int.Parse(Console.ReadLine());
+    if (n == 0) break;
+    if (n > max)
+        max = n;
+}
+System.Console.WriteLine(max);
