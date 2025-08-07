e4d86eee5750f8b3bb8394fd55c91f68c5fa122f
ismoil00-s <d.sievush@gmail.com>
2025-08-07 13:50:06 +0500

    task2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿int max = int.MinValue;
-int n;
-while (true)
+﻿// int max = int.MinValue;
+// int n;
+// while (true)
+// {
+//     n = int.Parse(Console.ReadLine());
+//     if (n == 0) break;
+//     if (n > max)
+//         max = n;
+// }
+// System.Console.WriteLine(max);
+
+//Task2
+
+int a = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+double s = 0;
+
+for (int i = 0; i <= n; i++)
 {
-    n = int.Parse(Console.ReadLine());
-    if (n == 0) break;
-    if (n > max)
-        max = n;
+    s = s + Math.Pow(a, i);
 }
-System.Console.WriteLine(max);
+System.Console.WriteLine(s);
